b9d1cffa0aa7546dc4f0f16645289c8cbe4606c6
Dmitriy Shishkin <6ast3r@gmail.com>
2021-10-01 12:44:10 +0700

    Render 3 tabs

--- App.js
+++ App.js
@@ -1,112 +1,51 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {useState, useRef, useCallback} from 'react';
+import {StyleSheet, View, Text, Button} from 'react-native';
+import PagerView from 'react-native-pager-view';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export default () => {
+  const [index, setIndex] = useState(0);
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const ref = useRef(null);
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const tryAgain = useCallback(() => {
+    setIndex(ind => ind + 1);
+  }, []);
+
+  const scrollToFirstPage = useCallback(() => {
+    ref.current?.setPage(0);
+  }, []);
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View key={index} style={styles.container}>
+      <View style={styles.buttons}>
+        <Button title="Try again" onPress={tryAgain} />
+        <Button title="Scroll to first page" onPress={scrollToFirstPage} />
+      </View>
+      <PagerView style={styles.pagerView} initialPage={2} ref={ref}>
+        <View key="0">
+          <Text>First page</Text>
+        </View>
+        <View key="1">
+          <Text>Second page</Text>
+        </View>
+        <View key="2">
+          <Text>Third page</Text>
+        </View>
+      </PagerView>
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    marginTop: 88,
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  pagerView: {
+    flex: 1,
   },
 });
-
-export default App;
